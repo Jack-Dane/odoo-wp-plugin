af72a6a12ee71ac13659f3b0b633938454ac4cff
Jack Dane <jackd98.jd@gmail.com>
2022-09-03 19:51:33 +0100

    Adding an update endpoint for form mappings

--- admin/api/endpoints/odoo_form_mappings.php
+++ admin/api/endpoints/odoo_form_mappings.php
@@ -8,6 +8,7 @@ class GetOdooFormMappings extends GetBaseSchema {
 
 }
 
+
 class PostOdooFormMappings extends PostBaseSchema { 
 	
 	protected function get_table_name () {
@@ -28,6 +29,23 @@ class PostOdooFormMappings extends PostBaseSchema {
 
 }
 
+
+class PutOdooFormMappings extends PutBaseSchema {
+	
+	protected function get_table_name () {
+		return "odoo_conn_form_mapping";
+	}
+
+	protected function update_data ($data) {
+		return array(
+			"odoo_form_id" => $data["odoo_form_id"],
+			"cf7_field_name" => $data["cf7_field_name"],
+			"odoo_field_name" => $data["odoo_field_name"]
+		);
+	}
+}
+
+
 function get_odoo_from_mappings ($data) {
 	$get_odoo_form_mappings = new GetOdooFormMappings();
 	$response = $get_odoo_form_mappings->request($data);
@@ -40,17 +58,27 @@ function create_odoo_form_mapping ($data) {
 	return $response;
 }
 
+function update_odoo_form_mapping ($data) {
+	$id = $data["id"];
+	$put_odoo_form_mappings = new PutOdooFormMappings($id);
+	$response = $put_odoo_form_mappings->request($data);
+	return $response;
+}
+
 add_action ( "rest_api_init", function () {
 	register_rest_route ( "odoo-conn/v1", "/get-odoo-form-mappings", array(
 		"methods" => "GET",
 		"callback" => "get_odoo_from_mappings"
 	));
-});
 
-add_action( "rest_api_init", function () {
 	register_rest_route ( "odoo-conn/v1", "/create-odoo-form-mapping", array(
 		"methods" => "POST",
 		"callback" => "create_odoo_form_mapping",
+	));
+
+	register_rest_route ( "odoo-conn/v1", "/update-odoo-form-mapping", array(
+		"methods" => "PUT",
+		"callback" => "update_odoo_form_mapping",
 	));
 });
 
