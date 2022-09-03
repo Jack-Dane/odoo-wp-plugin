35b7061956f5cf9800bf22e0f08df1c9a52ff38f
Jack Dane <jackd98.jd@gmail.com>
2022-09-03 18:29:26 +0100

    Commonise js table editor

--- admin/php/pageHelpers/table_display.php
+++ admin/php/pageHelpers/table_display.php
@@ -1,9 +1,11 @@
 <?php
 
 // register styles used
-add_action('admin_enqueue_scripts', 'callback_for_setting_up_scripts');
+add_action( "admin_enqueue_scripts", "callback_for_setting_up_scripts" );
 function callback_for_setting_up_scripts() {
     wp_enqueue_style( "table-style", plugins_url("/odoo-conn/admin/php/pageHelpers/table_style.css") );
+    wp_register_script('table-editor', plugins_url("/odoo-conn/admin/php/pageHelpers/table_editor.js"), array('jquery'), '1.0.0', true);
+    wp_enqueue_script( "table-editor" );
 }
 
 abstract class TableData {
--- admin/php/pages/odoo_form_mapping.php
+++ admin/php/pages/odoo_form_mapping.php
@@ -46,25 +46,6 @@ function odoo_form_mapping_page () {
 		}
 	}
 
-	jQuery(".table-row").click(function () {
-		let id = jQuery(this).attr('id');
-		if (jQuery(this).attr("data-update-state") != "true") {
-			jQuery(this).attr("data-update-state", "true");
-			jQuery(this).text("Save");
-			jQuery("." + id).each(function () {
-				let text = jQuery(this).text();
-				jQuery(this).replaceWith("<input type='text' class='" + id + "' value='" + text + "'/>");
-			});
-		} else {
-			jQuery(this).attr("data-update-state", "false");
-			jQuery(this).text("Edit");
-			jQuery("." + id).each(function () {
-				let text = jQuery(this).val();
-				jQuery(this).replaceWith("<span class='" + id + "'>" + text + "</span>");
-			});
-		}
-	});
-
 </script>
 <?php
 }
