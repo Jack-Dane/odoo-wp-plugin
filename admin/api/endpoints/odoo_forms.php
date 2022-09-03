8749e1b2439484f3c8a8619678df4d07be5ed1fa
Jack Dane <jackd98.jd@gmail.com>
2022-09-03 20:01:21 +0100

    Adding an update endpoint for odoo form

--- admin/api/endpoints/odoo_forms.php
+++ admin/api/endpoints/odoo_forms.php
@@ -29,6 +29,22 @@ class PostOdooForm extends PostBaseSchema {
 
 }
 
+class PutOdooForm extends PutBaseSchema {
+
+	protected function get_table_name () {
+		return "odoo_conn_form";
+	}
+
+	protected function update_data ($data) {
+		return array(
+			"odoo_connection_id" => $data["odoo_connection_id"],
+			"name" => $data["name"],
+			"contact_7_id" => $data["contact_7_id"]
+		);
+	}
+
+}
+
 function get_odoo_forms ($data) {
 	$get_odoo_connection = new GetOdooForm();
 	$response = $get_odoo_connection->request($data);
@@ -41,17 +57,27 @@ function create_odoo_form ($data) {
 	return $response;
 }
 
+function update_odoo_form ($data) {
+	$id = $data["id"];
+	$put_odoo_connection = new PutOdooForm($id);
+	$response = $put_odoo_connection->request($data);
+	return $response;
+}
+
 add_action( "rest_api_init", function () {
 	register_rest_route ( "odoo-conn/v1", "/create-odoo-form", array(
 		"methods" => "POST",
 		"callback" => "create_odoo_form",
 	));
-});
 
-add_action ( "rest_api_init", function () {
 	register_rest_route ( "odoo-conn/v1", "/get-odoo-forms", array(
 		"methods" => "GET",
 		"callback" => "get_odoo_forms"
+	));
+
+	register_rest_route ( "odoo-conn/v1", "/update-odoo-form", array(
+		"methods" => "PUT",
+		"callback" => "update_odoo_form"
 	));
 });
 
