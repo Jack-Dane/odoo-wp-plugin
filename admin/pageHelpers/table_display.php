b461f7febd593fba02d264de34aee87921c9bb39
Jack Dane <jackd98.jd@gmail.com>
2023-11-13 21:45:25 +0000

    Remove the old table display scripts and styles
    * They have been replaced with the default wordpress display.

--- admin/pageHelpers/table_display.php
+++ admin/pageHelpers/table_display.php
@@ -1,27 +1,9 @@
 <?php
 
+
 if (!class_exists('WP_List_Table')) {
     require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
 }
-
-// register styles & scripts used
-add_action("admin_enqueue_scripts", __NAMESPACE__ . "odoo_conn_page_scripts_callback");
-
-function odoo_conn_page_scripts_callback()
-{
-    $root = esc_url_raw(rest_url());
-    $nonce = wp_create_nonce("wp_rest");
-    wp_register_script(
-        "table-display", plugins_url("table_display.js", __FILE__), array("jquery"), "1.0.1", true
-    );
-    wp_localize_script("table-display", "wpApiSettings", array(
-        "root" => $root, "nonce" => $nonce
-    ));
-    wp_enqueue_script("table-display");
-
-    wp_enqueue_style("odoo-page-style", plugins_url("page_style.css", __FILE__));
-}
-
 
 class OdooConnCustomTableDeletableDisplay extends WP_List_Table {
 
