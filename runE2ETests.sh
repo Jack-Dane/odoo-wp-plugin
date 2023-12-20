45ad69009f30788a1c7f4ff333cec358ba0b4b05
Jack Dane <jackd98.jd@gmail.com>
2023-12-20 20:19:29 +0000

    Fix delete tests

--- runE2ETests.sh
+++ runE2ETests.sh
@@ -32,7 +32,7 @@ docker exec $dockerPHPContainerId /bin/sh -c 'cd /usr/src/odoo_conn; vendor/bin/
 docker exec $dockerPHPContainerId /bin/sh -c 'cd /usr/src/odoo_conn; vendor/bin/phpunit tests/end_to_end_tests/tests/selenium_tests/SendData_Test.php'
 docker exec $dockerPHPContainerId /bin/sh -c 'cd /usr/src/odoo_conn; vendor/bin/phpunit tests/end_to_end_tests/tests/selenium_tests/Error_Test.php'
 docker exec $dockerPHPContainerId /bin/sh -c 'cd /usr/src/odoo_conn; vendor/bin/phpunit tests/end_to_end_tests/tests/selenium_tests/Update_Test.php'
-#docker exec $dockerPHPContainerId /bin/sh -c 'cd /usr/src/odoo_conn; vendor/bin/phpunit tests/end_to_end_tests/tests/selenium_tests/Delete_Test.php'
+docker exec $dockerPHPContainerId /bin/sh -c 'cd /usr/src/odoo_conn; vendor/bin/phpunit tests/end_to_end_tests/tests/selenium_tests/Delete_Test.php'
 
 # cleanup the containers
 docker compose -f tests/end_to_end_tests/odoo_compose.yaml down
