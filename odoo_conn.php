24cb3b68cafe3a1a37af03b32a29e4afc67d6e48
Jack Dane <jackd98.jd@gmail.com>
2024-06-06 16:40:21 +0100

    Remove the CF7 dependency check.
    * The "Required Plugins" header will make sure the contact form 7 plugin is installed.

--- odoo_conn.php
+++ odoo_conn.php
@@ -24,9 +24,6 @@ require_once 'vendor/autoload.php';
 require_once 'encryption.php';
 require_once 'odoo_connector/odoo_connector.php';
 
-require_once 'dependency_check.php';
-register_activation_hook( __FILE__, '\\odoo_conn\\dependency_check\\odoo_conn_contact_7_plugin_active' );
-
 require_once 'activation.php';
 register_activation_hook( __FILE__, 'odoo_conn_activation_function' );
 
