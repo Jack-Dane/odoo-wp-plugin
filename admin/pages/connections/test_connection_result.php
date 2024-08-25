2617226bbb1bb3e8bc19456f5da8963d4b3a302f
Jack Dane <jackd98.jd@gmail.com>
2024-08-25 16:50:36 +0100

    Add some extra styling to the test connection message
    * Colour makes the message easier to understand at a glance.

--- admin/pages/connections/test_connection_result.php
+++ admin/pages/connections/test_connection_result.php
@@ -1,10 +1,12 @@
-<div class="wrap odoo-conn-message-notification-box">
-    <?php $test_result = $test_result ?? null ?>
+<?php
+$test_result = $test_result ?? null;
+if (!is_null($test_result)) {
+	$connection_result_class = $test_result['success'] ? 'test-result-ok' : 'test-result-failure'
+	?>
+    <div class="wrap odoo-conn-message-notification-box <?= $connection_result_class ?>">
+        <h1>Test Result: <?= $test_result['success'] ? 'Success' : 'Failure' ?> </h1>
 
-    <?php if (!is_null($test_result)) { ?>
-        <h1>Test Result: <?= $test_result['success'] ? "Success" : "Failure" ?> </h1>
-
-        <?php if (!$test_result['success']) { ?>
+		<?php if (!$test_result['success']) { ?>
             <!-- Display error details on failure -->
             <div>
                 <b>Error Message: </b>
@@ -15,6 +17,6 @@
                 <b>Error Code: </b>
                 <span><?= esc_html__($test_result['error_code']) ?></span>
             </div>
-        <?php } ?>
-    <?php } ?>
-</div>
+		<?php } ?>
+    </div>
+<?php } ?>
